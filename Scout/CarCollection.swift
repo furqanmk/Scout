9469fa2f4cb95fd40911a7c5288dd2e18586c6f6
Furqan Muhammad Khan <furqanmk@outlook.com>
2017-06-14 17:30:51 +0500

    Implemented the CarCollection class that fetches JSON and parses into the model.

---
+++ Scout/CarCollection.swift
@@ -0,0 +1,57 @@
+//
+//  MovieList.swift
+//  Scout
+//
+//  Created by Furqan on 14/06/2017.
+//  Copyright © 2017 Furqan Muhammad Khan. All rights reserved.
+//
+
+import Foundation
+import SwiftyJSON
+
+protocol CarCollectionDelegate {
+    func didLoadCars()
+}
+
+class CarCollection {
+    static let shared = CarCollection()
+    private init() { }
+    
+    var delegate: CarCollectionDelegate?
+    
+    // Holds all the cars
+    var list: [Car] = [ ]
+    
+    // Currently active request
+    var request: DownloadRequest<JSON>!
+    
+    func fetch() {
+        if request != nil {
+            request.cancel()
+        }
+        self.request = DownloadRequest<JSON>(path: "http://sumamo.de/iOS-TechChallange/api/index/make=all.json",
+        progress: nil,
+        success: { [weak self] json in
+            guard let cars = json["vehicles"].array else {
+                return
+            }
+            self?.list.removeAll()
+            
+            for item in cars {
+                let car = Car(json: item)
+                self?.list.append(car)
+            }
+            self?.delegate?.didLoadCars()
+            self?.request = nil
+        },
+        failure: { [weak self] error in
+            print("Error loading movies.")
+            self?.request = nil
+        })
+    }
+    
+    func clear() {
+        list = [ ]
+        DownloadImageManager.shared.cache.clearCache()
+    }
+}
